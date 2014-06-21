d7b7c386e4553400fd36135fe18ac333f8918087
wpassfield <williampassfield@gmail.com>
2014-06-21 18:13:30 +0100

    cachematrix R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){##creates list of functions which is subset in cacheSolve function
+	invmatrix <- NULL
+	set <- function(y) {
+                x <<- y
+                invmatrix <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) invmatrix <<- solve
+        getinv <- function() invmatrix
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {##if matrix inverse has not been calculated, calculates matrix inverse and stores in global envinroment. If calculated previously retrieves previous result from global environment
+        invmatrix <- x$getinv()
+        if(!is.null(invmatrix)) {
+                message("getting cached data")
+                return(invmatrix)
+        }
+        data <- x$get()
+        invmatrix <- solve(data, ...)
+        x$setinv(invmatrix)
+        invmatrix
 }
